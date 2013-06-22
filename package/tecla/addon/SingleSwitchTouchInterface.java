11f5dc479fc5bfd33ca9714259b896c2daa9f9f6
Eric Wan <eric.wan@komodoopenlab.com>
2013-06-22 09:30:57 -0400

    Changed HUD disabling method to a left-to-right swipe gesture.

--- package/tecla/addon/SingleSwitchTouchInterface.java
+++ package/tecla/addon/SingleSwitchTouchInterface.java
@@ -2,6 +2,8 @@ package com.android.tecla.addon;
 
 import ca.idrc.tecla.R;
 import android.content.Context;
+import android.graphics.Point;
+import android.view.Display;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -17,6 +19,8 @@ public class SingleSwitchTouchInterface extends SimpleOverlay {
 	 */
 	public static final String CLASS_TAG = "SingleSwitchTouchInterface";
 	private static SingleSwitchTouchInterface sInstance;
+	
+	private int mTouchDownCoordX, mTouchDownCoordY;
 
 	public SingleSwitchTouchInterface(Context context) {
 		super(context);
@@ -29,8 +33,8 @@ public class SingleSwitchTouchInterface extends SimpleOverlay {
 		rView.setBackgroundResource(R.drawable.screen_switch_background_normal);
 		rView.setOnTouchListener(mOverlayTouchListener);
 		
-		if(!TeclaApp.persistence.isInverseScanningEnabled()) 
-			setLongClick(true);
+		/*if(!TeclaApp.persistence.isInverseScanningEnabled()) 
+			setLongClick(true);*/
 		
 	}
 
@@ -50,14 +54,42 @@ public class SingleSwitchTouchInterface extends SimpleOverlay {
 	private View.OnTouchListener mOverlayTouchListener = new View.OnTouchListener() {
 		
 		public boolean onTouch(View v, MotionEvent event) {
+			
+			// get display resolution
+			WindowManager wm = (WindowManager) sInstance.getContext().getSystemService(Context.WINDOW_SERVICE);
+			Display display = wm.getDefaultDisplay();
+			Point size = new Point();
+			display.getSize(size);
+			int width = size.x;
+			int height = size.y;
+			
 			switch (event.getAction()) {
 			case MotionEvent.ACTION_DOWN:
+				
+				// use this method to disable HUD
+				sInstance.mTouchDownCoordX = (int) event.getX();
+				sInstance.mTouchDownCoordY = (int) event.getY();
+				
 				sInstance.getRootView().setBackgroundResource(R.drawable.screen_switch_background_pressed);
 				TeclaApp.a11yservice.injectSwitchEvent(
 						new SwitchEvent(SwitchEvent.MASK_SWITCH_E1, 0)); //Primary switch pressed
 				// if (TeclaApp.DEBUG) Log.d(TeclaApp.TAG, CLASS_TAG + "Fullscreen switch down!");
 				break;
 			case MotionEvent.ACTION_UP:
+				
+				// use this method to disable HUD
+				int coordX = (int) event.getX();
+				int coordY = (int) event.getY();
+				if(sInstance.mTouchDownCoordX < width/2
+						&& coordX > width/2) {
+					sInstance.getRootView().setBackgroundResource(R.drawable.screen_switch_background_normal);
+					TeclaApp.persistence.setFullscreenEnabled(false);
+					TeclaApp.getInstance().turnFullscreenOff();
+					TeclaStatic.logV(CLASS_TAG, "Disabled HUD");
+					return false;
+				}
+					
+				
 				sInstance.getRootView().setBackgroundResource(R.drawable.screen_switch_background_normal);
 				TeclaApp.a11yservice.injectSwitchEvent(
 						new SwitchEvent(0,0)); //Switches released
